c332dd850240585d835e1ceee4ac55b7ca30f342
MarkovicMaja1 <majamarkovic.b@gmail.com>
2024-02-07 14:29:14 +0100

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,122 @@
+
+
+export const projectExperience = [
+  {
+    name: "Education",
+    projects: "School of Bussines, Computer Science ",
+    language: "2019 - 2022 (Bacherol's degree)",
+    
+    bg: "#286F6C",
+  },
+  {
+    name: "Work experience",
+    projects: "1 year of experience in web development, mostly in JavaScript and React",
+  
+    bg: "#EEC048",
+  },
+  {
+    name: "Languages",
+    projects: `English-Profesional Working Profiency `,
+    language: ' German-Profesional Working Profiency',
+
+    bg: "#F26440",
+  },
+];
+
+export const WhatDoIHelp = [
+  "I am Full Stack developer, more focused on Front end. I have experience in building application using React, Angular, NodeJS and other familiar technologies and tools.",
+];
+
+export const workExp = [
+  {
+    place: "Self-Employed, Brisbane",
+    tenure: "Aug 2014 - Sep 2016",
+    role: "Visual Designer",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+  {
+    place: "New Man Services",
+    tenure: "Aug 2014 - Sep 2016",
+    role: "UI/UX Designer",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+  {
+    place: "Global Solution",
+    tenure: "Aug 2014 - Sep 2016",
+    role: "Sr. Product Designer",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+];
+
+export const comments = [
+  {
+    name: "Aleksandar Vrbaski",
+    post: "CEO Loads King doo",
+    comment:
+      "I emphasize that Maja Marković easily fit into the team during her engagement in the Company contributed to the functioning of the collective. Whenever the need arose, she demonstrated willingness to help colleagues in performing work tasks. With your knowledge, your effort and commitment, she significantly contributed to the successful functioning of the sector.Bearing in mind the outstanding qualities that Maja Marković has shown during her career engagement in the Company, I recommend with pleasure as a valuable, dedicated, a responsible and trustworthy person who will undoubtedly contribute with his work to anyone collective",
+   
+  },
+  {
+    name: "Predrag Ranitovic",
+    post: "Professor Doctor",
+    comment:
+      "Among the qualities possessed by my colleague Maja Marković, I think it is particularly important to point out her communicativeness, readiness for teamwork, initiative, respect for the knowledge of those better than herself and the desire to surpass them. The work habits demonstrated so far represent her as a hardworking student and are only an indication of the results that can be expected from her in the future. Based on everything stated above, I believe that Maja Marković has the potential to be a good, devoted worker and colleague in every work unit.",
+   
+  },
+  {
+    name: "Predrag Ranitovic",
+    post: "Professor Doctor",
+    comment:
+      "Among the qualities possessed by my colleague Maja Marković, I think it is particularly important to point out her communicativeness, readiness for teamwork, initiative, respect for the knowledge of those better than herself and the desire to surpass them. The work habits demonstrated so far represent her as a hardworking student and are only an indication of the results that can be expected from her in the future. Based on everything stated above, I believe that Maja Marković has the potential to be a good, devoted worker and colleague in every work unit.",
+   
+  },
+  {
+    name: "Predrag Ranitovic",
+    post: "Professor Doctor",
+    comment:
+      "Among the qualities possessed by my colleague Maja Marković, I think it is particularly important to point out her communicativeness, readiness for teamwork, initiative, respect for the knowledge of those better than herself and the desire to surpass them. The work habits demonstrated so far represent her as a hardworking student and are only an indication of the results that can be expected from her in the future. Based on everything stated above, I believe that Maja Marković has the potential to be a good, devoted worker and colleague in every work unit.",
+   
+  },
+ 
+];
+
+export const sliderSettings = {
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  touchMove: true,
+  useCSS: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
